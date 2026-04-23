e1b58b0365883aaa5afb95badc4d9fb5961717f5
0xMichaelRan <michaelran16@gmail.com>
2026-04-23 19:41:46 +0800

    add self-update script

--- scripts/ytdl.sh
+++ scripts/ytdl.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+# Self-Update Script
+if [[ "$1" == "--update" ]]; then
+    echo "Updating downloadyoutube..."
+    curl -sL https://github.com/rhythwitty/bashrepo/raw/main/scripts/downloadyoutube.sh -o downloadyoutube
+    chmod +x downloadyoutube
+    sudo mv downloadyoutube /usr/local/bin/downloadyoutube
+    echo "Update complete!"
+    exit 0
+fi
+
 # A Mac-friendly downloader script
 URL=$1
 if [ -z "$URL" ]; then
-    echo "Usage: ytdl <URL>"
+    echo "Usage: downloadyoutube <URL>"
     return 1 2>/dev/null || exit 1
 fi
 
